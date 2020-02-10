2d64fd10d7ea69a906c029ce6b4401cc0abbaa75
Ryan Seit <ryan@ryanseit.com>
2020-02-10 21:29:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,16 +11,16 @@ def directors_totals(nds)
   result = {
   }
   outer_index = 0
-  director = nds[outer_index][:name]
   while outer_index < nds.length do
     inner_index = 0
+    director = nds[outer_index][:name]
     total = 0
     while inner_index < nds[outer_index][:movies].length do
       movie_gross = nds[outer_index][:movies][inner_index][:worldwide_gross]
       total += movie_gross.to_i
       inner_index += 1
     end
-    result[director] = total
+    result.merge![director] = total
     outer_index += 1
   end
   #
