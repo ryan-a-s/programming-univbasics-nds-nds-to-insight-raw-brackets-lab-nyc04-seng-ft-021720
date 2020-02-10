9217da3a1b9037a3f5b4c67b742f87a758173eb7
Ryan Seit <ryan@ryanseit.com>
2020-02-10 21:03:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,7 +20,7 @@ def directors_totals(nds)
       total += movie_gross
       inner_index += 1
     end
-    result[outer_index] = {"#{nds[outer_index][:name]}" => total}
+    result = {"#{nds[outer_index][:name]}" => total}
     puts result
     outer_index += 1
   end
